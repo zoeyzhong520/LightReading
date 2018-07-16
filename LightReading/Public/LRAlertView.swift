a375274945241183e5084d235689d2b12ba54936
AsiaZhang <17612156438@sina.cn>
2018-07-16 18:46:22 +0800

    修改LRAlertView类

--- LightReading/Public/LRAlertView.swift
+++ LightReading/Public/LRAlertView.swift
@@ -31,15 +31,40 @@ class LRAlertView: UIView {
         self.backgroundColor = MongolianlayerColor
         
         if self.contentView == nil {
-            self.contentView = UIView(frame: CGRect(x: 0, y: ScreenHeight-ContentViewH, width: ScreenWidth, height: ContentViewH))
+            self.contentView = UIView(frame: CGRect(x: 0, y: ScreenHeight, width: ScreenWidth, height: ContentViewH))
             self.addSubview(self.contentView!)
             
-            
+            self.configContentView()
         }
     }
 
     func configContentView() {
-        
+        if self.buttonsArray.isEmpty == false {
+            //取消
+            let cancelBtn = UIButton.init("取消", textColor: LightGrayColor, font: FourthFont, backgroundColor: .white, target: self, action: #selector(clickAction(_:)))
+            cancelBtn.tag = 888
+            self.contentView?.addSubview(cancelBtn)
+            cancelBtn.snp.makeConstraints({ (make) in
+                make.bottom.left.right.equalToSuperview()
+                make.height.equalTo(fontSizeScale(30))
+            })
+            
+            for i in 0..<self.buttonsArray.count {
+                let btnW = ScreenWidth
+                let btnH = fontSizeScale(30)
+                
+                
+            }
+        }
+    }
+    
+    @objc func clickAction(_ button:UIButton) {
+        if button.tag == 888 {
+            //取消按钮
+            
+        } else {
+            
+        }
     }
 }
 
