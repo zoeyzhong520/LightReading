cb2031817190210bf050663b84c649895b2b4153
AsiaZhang <17612156438@sina.cn>
2018-07-18 18:05:18 +0800

    添加TopTitleModel

---
+++ LightReading/Home（首页）/TopTitleScrollBar（顶部标题滚动条）/Model/TopTitleModel.swift
@@ -0,0 +1,13 @@
+//
+//  TopTitleModel.swift
+//  LightReading
+//
+//  Created by zhifu360 on 2018/7/18.
+//  Copyright © 2018年 智富金融. All rights reserved.
+//
+
+import UIKit
+
+class TopTitleModel: NSObject {
+
+}
